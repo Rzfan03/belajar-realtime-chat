7f516c11ee093559d04d5df3858d8a512cf935c6
Zennius! <fajrulsyaban47@gmail.com>
2026-01-16 14:00:38 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const supabase = require('@supabase/supabase-js')
@@ -6,11 +6,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 const PORT = 3000
-const SUPABASE_URL = "https://qlmbvfjcpbnxarofnpry.supabase.co"
-const SUPABASE_KEY = "sb_secret_vyxpcdE6-vQ0RkduVI-9Wg_wAaNG6l3"
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 
-const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+
+const db = supabase.createClient(supabaseUrl, supabaseKey)
 app.get('/', async(req, res) => {
   const getData = await db.from('users').select()
   console.log(getData)
